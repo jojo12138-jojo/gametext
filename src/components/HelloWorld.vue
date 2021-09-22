3f1ed089debc868a7621fe8039e7b1ff9dbaceda
spiderman <diotangxh@foxmail.com>
2021-09-22 15:54:34 +0800

    修改配置

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1 class="font-semibold text-4xl text-purple-700">{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
